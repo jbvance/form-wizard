c3f2f5c8257a84d642c072ee5938b212907884e3
Jason Vance <jbvance@gmail.com>
2017-02-18 07:02:47 -0600

    Add lightbox functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+          disableOn: 700,
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: false,
+
+          fixedContentPos: false
+        });
   //alert("Ready");
 
 });
